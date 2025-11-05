699eea2a3bdc88d0d8ece1b92029855faa84ebae
Narayanaacademy <academyrunnarayana@gmail.com>
2025-11-06 00:10:44 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,9 @@
-// next-sitemap.config.js
-const siteUrl = "https://yourdomain.com";
+const siteUrl = "https://yourdomain.com"; // replace with your actual URL
 
 export default {
   siteUrl,
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  changefreq: "weekly",
+  priority: 0.7
 };
