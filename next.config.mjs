3dac2239710065763cca956ce4f32ccfe2853a58
Narayanaacademy <academyrunnarayana@gmail.com>
2025-11-06 00:30:31 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,16 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+/** Get __dirname in ESM */
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true
   },
-  outputFileTracingRoot: __dirname, // prevents workspace root warning
+  outputFileTracingRoot: __dirname, // fixes workspace root warning
   images: {
     unoptimized: false
   }
