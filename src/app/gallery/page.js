9aa707c7ca93d6d8217cc16b959d71494caf1180
Narayanaacademy <academyrunnarayana@gmail.com>
2025-11-06 14:20:11 +0530

    Update page.js

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
