c10f7898f65bf70d5458a8877e46f5b6c5271bd6
Narayanaacademy <academyrunnarayana@gmail.com>
2025-11-10 09:27:26 +0530

    Create sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,30 @@
+export default async function sitemap() {
+  const baseUrl = "https://www.narayanaathletics.in";
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/gallery`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/resume`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+  ];
+}
